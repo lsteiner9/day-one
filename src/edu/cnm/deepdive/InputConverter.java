3d3dadffed2431e0b2085db91035e0c256008ee7
lsteiner9 <lsteiner1@cnm.edu>
2020-09-21 15:00:40 -0600

    Several changes to clean up Flowgorithm source code; documentation.

--- src/edu/cnm/deepdive/InputConverter.java
+++ src/edu/cnm/deepdive/InputConverter.java
@@ -1,21 +1,21 @@
 package edu.cnm.deepdive;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class InputConverter {
-    private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         boolean running = true;
+
         while (running) {
             String userInput;
             
             System.out.println("Enter a temperature in degrees Celsius (blank to quit).");
             userInput = input.nextLine();
+
             if (userInput.length() > 0) {
-                double celsius;
-                
-                celsius = Double.parseDouble(userInput);
+                double celsius = Double.parseDouble(userInput);
                 System.out.print("Temperature in degrees Fahrenheit = ");
                 System.out.println(celsiusToFahrenheit(celsius));
             } else {
